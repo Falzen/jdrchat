f66ba65934d0b94cd61f4e0325f9c9701aa1ad01
Falzen <jerome.malenfant@gmail.com>
2019-10-22 01:51:23 +0200

    config.php online

--- php/config.php
+++ php/config.php
@@ -1,8 +1,9 @@
 <?php
 	function getConn() {
 		try {
-			$db = new PDO('mysql:host=localhost;dbname=jdr;charset=utf8', 'root', '');
-			//$db = new PDO('mysql:host=db762956775.hosting-data.io;dbname=db762956775;charset=utf8', 'dbo762956775', 'epingler1Pingouin!');			$db->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER); // always lower case.
+			//$db = new PDO('mysql:host=localhost;dbname=jdr;charset=utf8', 'root', '');
+			$db = new PDO('mysql:host=db5000202677.hosting-data.io;dbname=dbs197639;charset=utf8', 'dbu158846', 'Printf!2501');
+			$db->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER); // always lower case.
 			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // errors will trigger exceptions.
 			$db->exec('SET NAMES utf8');
 			return $db;
