ac4be774cdebf34b78ca08c43eb6860deb833746
j-malenfant <jerome.malenfant@modis.com>
2019-10-22 09:55:25 +0200

    ajout des txt de bienvenue

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php
 	function getConn() {
 		try {
-			//$db = new PDO('mysql:host=localhost;dbname=jdr;charset=utf8', 'root', '');
-			$db = new PDO('mysql:host=db5000202677.hosting-data.io;dbname=dbs197639;charset=utf8', 'dbu158846', 'Printf!2501');
+			$db = new PDO('mysql:host=localhost;dbname=jdrchat;charset=utf8', 'root', '');
+			//$db = new PDO('mysql:host=db5000202677.hosting-data.io;dbname=dbs197639;charset=utf8', 'dbu158846', 'Printf!2501');
 			$db->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER); // always lower case.
 			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // errors will trigger exceptions.
 			$db->exec('SET NAMES utf8');
